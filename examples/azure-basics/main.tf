5257018c03153b320bd8b716a0ad6c56a7fea1bb
Brian Thompson <bthompson@dhcp-9-16-39-70.atx-us.ibm.com>
2026-06-18 16:01:12 -0500

    Adding Azure functionality (untested)

---
+++ examples/azure-basics/main.tf
@@ -0,0 +1,91 @@
+provider "azurerm" {
+  features {}
+}
+
+# -------------------
+# Resource Group
+# -------------------
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-main"
+  location = "East US"
+}
+
+# -------------------
+# Virtual Network (equivalent to VPC)
+# -------------------
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-main"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# -------------------
+# Subnet (inside VNet)
+# -------------------
+
+resource "azurerm_subnet" "public" {
+  name                 = "subnet-public"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# -------------------
+# Public IP (equivalent to Internet Gateway exposure)
+# -------------------
+
+resource "azurerm_public_ip" "web" {
+  name                = "pip-web"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+}
+
+# -------------------
+# Network Interface (required by Azure VM)
+# -------------------
+
+resource "azurerm_network_interface" "web" {
+  name                = "nic-web"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.public.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web.id
+  }
+}
+
+# -------------------
+# Linux VM (equivalent to EC2 t3.micro)
+# -------------------
+
+resource "azurerm_linux_virtual_machine" "web" {
+  name                = "vm-web"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_D2ls_v7"
+
+  admin_username                  = "adminuser"
+  disable_password_authentication = false
+  admin_password                  = "kycvi5-fipwyf-berruH"
+
+  network_interface_ids = [azurerm_network_interface.web.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
